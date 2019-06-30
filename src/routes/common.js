447fa143e1033a7f8eb5f03d17380393f74affa8
akiokio <akio.xd@gmail.com>
2019-06-29 17:58:41 -0700

    feat(common/home): add route example pt 2

--- src/routes/common.js
+++ src/routes/common.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  home,
-  one,
-  post,
-  downloader,
-  reader
-} = require("../controllers/common");
+const { home } = require("../controllers/common");
 
 router.get("/", home);
-router.get("/one", one);
-router.get("/downloader", downloader);
-router.get("/reader", reader);
-
-router.post("/", post);
 
 module.exports = router;
