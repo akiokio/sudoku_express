38262deac6031365ff24a529b61cfdaa22d27547
akiokio <akio.xd@gmail.com>
2019-06-30 23:41:31 -0700

    feat(game-api): add updateBoard endpoint without validation

--- src/controllers/sudoku.js
+++ src/controllers/sudoku.js
@@ -5,14 +5,15 @@ const models = require("../models");
 
 const home = asyncMiddleware(async (req, res) => {
   const games = await models.Game.findAll();
-  res.json(
-    games.map(game => ({
+  res.json({
+    success: true,
+    games: games.map(game => ({
       [game.slug]: {
         name: game.name,
         url: game.getPlayUrl(req)
       }
     }))
-  );
+  });
 });
 
 const start = asyncMiddleware(async (req, res) => {
@@ -27,31 +28,57 @@ const start = asyncMiddleware(async (req, res) => {
     throw error;
   }
 
-  game.printBoard();
   res.json({
+    success: true,
     url: game.getPlayUrl(req),
     game
   });
 });
 
 const play = asyncMiddleware(async (req, res) => {
-  const games = await models.Game.findAll({
+  const [game] = await models.Game.findAll({
     where: {
       id: req.params.id
     }
   });
-  res.json(
-    games.map(game => ({
-      [game.slug]: {
-        url: game.getPlayUrl(req),
-        board: game.board
-      }
-    }))
+  if (!game) {
+    res.status(404).json({ success: false, error: "Game not found" });
+  }
+  res.json({
+    success: true,
+    game: {
+      url: game.getPlayUrl(req),
+      board: game.board
+    }
+  });
+});
+
+const updateBoard = asyncMiddleware(async (req, res) => {
+  const row = req.body["row"];
+  const col = req.body["col"];
+  const val = req.body["val"];
+
+  if (!row || !col || !val) {
+    return res
+      .status(400)
+      .json({ success: false, error: "Row, col or val empty" });
+  }
+
+  // Results will be an empty array and metadata will contain the number of affected rows.
+  const [results, metadata] = await models.sequelize.query(
+    `UPDATE "Games" SET board[${row}][${col}] = ${val} WHERE id = '${
+      req.params.id
+    }';`
   );
+  res.json({
+    success: true,
+    affected: metadata.rowCount
+  });
 });
 
 module.exports = {
   home,
   start,
-  play
+  play,
+  updateBoard
 };
--- src/routes/sudoku.js
+++ src/routes/sudoku.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { home, start, play } = require("../controllers/sudoku");
+const { home, start, play, updateBoard } = require("../controllers/sudoku");
 
 router.get("/", home);
 router.get("/start", start);
 router.get("/play/:id", play);
+router.post("/play/:id/", updateBoard);
 
 module.exports = router;
